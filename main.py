c8359178fee2ef89a7cc3ca4f8ec4a3efa9f285d
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-05-01 11:15:18 -0300

    Saída do resultado para arquivo csv

--- main.py
+++ main.py
@@ -20,12 +20,15 @@ for p in range(3, 5):
         #número de repetições de cada experimento: 51
         for i in range(5):
             algo = algorithm(de(gen=500*d, F=0.8, CR=0.9, variant=2, ftol=1e-20, xtol=1e-20))
+            #todo: verificar por que colocar a mesma seed não está gerando o mesmo resultado
+            # para o mesmo problema
+            algo.set_seed(0)
             algo.set_verbosity(0)
             prob = problem(CECProb(d, p))
             pop = population(prob, 20)
             pop = algo.evolve(pop)
             uda = algo.extract(de)
-            results_str = results_str +"\n"+ f"{p}\t{d}\t{pop.champion_f[0]}"
+            results_str = results_str + f"{p}\t{d}\t{pop.champion_f[0]}\n"
             print(f"|", end="")
 
         print("]", end="")
@@ -35,7 +38,7 @@ for p in range(3, 5):
     print(f"Time elapsed: {end - start}")
         #print([x[2] for x in uda.get_log()])
 
+with open("result.csv", mode="w") as file:
+    file.write(results_str)
 
-
-results_str = results_str + "\n"
 print(results_str)
